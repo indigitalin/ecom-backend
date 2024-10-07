1c28023b506606616b2df2f2ecc754fdb8b1df20
Shefii-02 <shefii.km@gmail.com>
2024-10-07 17:17:59 +0530

    migration files

---
+++ database/migrations/2024_10_07_111755_create_clients_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('clients', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('user_id')->unsigned()->nullable();
+            $table->foreign('user_id')->references('id')->on('users')->onDelete("cascade");
+            $table->string('business_name');
+            $table->string('industry');
+            $table->string('description');
+            $table->string('address');
+            $table->string('city');
+            $table->bigInteger('state_id')->unsigned()->nullable();
+            $table->foreign('state_id')->references('id')->on('states')->onDelete("cascade");
+            $table->bigInteger('country_id')->unsigned()->nullable();
+            $table->foreign('country_id')->references('id')->on('countries')->onDelete("cascade");
+            $table->bigInteger('plan_id')->unsigned()->nullable();
+            $table->foreign('plan_id')->references('id')->on('plans')->onDelete("cascade");
+            $table->boolean('status')->default(1);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('clients');
+    }
+};
