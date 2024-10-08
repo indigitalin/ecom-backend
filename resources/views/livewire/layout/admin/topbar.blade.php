5b43c780a1d5a27a798cb2e6b673a42b23589f16
James <rohithr.indigital@gmail.com>
2024-10-08 13:59:51 +0530

    dashboard

--- resources/views/livewire/layout/admin/topbar.blade.php
+++ resources/views/livewire/layout/admin/topbar.blade.php
@@ -36,40 +36,41 @@ new class extends Component
                 </a>
             </div>
             <div class="flex items-center">
+                <livewire:layout.admin.notification />
                 <div class="flex items-center ms-3">
                     <div>
                         <button type="button"
                             class="flex text-sm bg-gray-800 rounded-full focus:ring-4 focus:ring-gray-300 dark:focus:ring-gray-600"
                             aria-expanded="false" data-dropdown-toggle="dropdown-user">
                             <span class="sr-only">Open user menu</span>
-                            <img class="w-8 h-8 rounded-full"
+                            <img class="w-10 h-10 rounded-full"
                                 src="{{ auth()->user()->avatar_url }}" alt="user photo">
                         </button>
                     </div>
-                    <div class="z-50 hidden my-4 text-base list-none bg-white divide-y divide-gray-100 rounded shadow dark:bg-gray-700 dark:divide-gray-600"
+                    <div class="z-50 hidden my-4 text-base list-none bg-white divide-y divide-gray-100 rounded shadow-none border dark:bg-gray-700 dark:divide-gray-600"
                         id="dropdown-user">
                         <div class="px-4 py-3" role="none">
-                            <p class="text-sm text-gray-900 dark:text-white" role="none">
+                            <p class="text-gray-900 font-bold dark:text-white" role="none">
                                 {{ auth()->user()->name }}
                             </p>
-                            <p class="text-sm font-medium text-gray-900 truncate dark:text-gray-300" role="none">
+                            <p class="font-medium text-gray-900 truncate dark:text-gray-300" role="none">
                                 {{ auth()->user()->email }}
                             </p>
                         </div>
                         <ul class="py-1" role="none">
                             <li>
-                                <a href="#"
-                                    class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:text-gray-300 dark:hover:bg-gray-600 dark:hover:text-white"
-                                    role="menuitem">Dashboard</a>
+                                <a href="{{ route('admin.profile') }}" wire:navigate
+                                    class="block px-4 py-2 text-gray-700 hover:bg-gray-100 dark:text-gray-300 dark:hover:bg-gray-600 dark:hover:text-white"
+                                    role="menuitem">Profile</a>
                             </li>
                             <li>
-                                <a href="#"
-                                    class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:text-gray-300 dark:hover:bg-gray-600 dark:hover:text-white"
-                                    role="menuitem">Settings</a>
+                                <a href="{{ route('admin.password') }}" wire:navigate
+                                    class="block px-4 py-2 text-gray-700 hover:bg-gray-100 dark:text-gray-300 dark:hover:bg-gray-600 dark:hover:text-white"
+                                    role="menuitem">Security</a>
                             </li>
                             <li>
                                 <a href="#" wire:click="logout"
-                                    class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:text-gray-300 dark:hover:bg-gray-600 dark:hover:text-white"
+                                    class="block px-4 py-2 text-gray-700 hover:bg-gray-100 dark:text-gray-300 dark:hover:bg-gray-600 dark:hover:text-white"
                                     role="menuitem">Sign out</a>
                             </li>
                         </ul>
--- routes/admin.php
+++ routes/admin.php
@@ -7,4 +7,6 @@ use Livewire\Volt\Volt;
 
 Route::middleware('auth')->prefix('/admin')->name('admin.')->group(function () {
     Route::view('/', 'admin.index')->name('index');
+    Route::view('/profile', 'admin.profile')->name('profile');
+    Route::view('/password', 'admin.password')->name('password');
 });
