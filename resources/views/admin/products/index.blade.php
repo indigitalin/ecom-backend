130b561ea5b2c225e179b9d76b10107e5963c737
James <rohithr.indigital@gmail.com>
2024-10-08 16:48:28 +0530

    struct change

--- resources/views/admin/products/index.blade.php
+++
@@ -1,9 +0,0 @@
-<x-app-layout>
-    <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-        <h2 class="text-xl text-gray-800 dark:text-gray-200 leading-tight mb-5">
-            {{ __('Products') }}
-        </h2>
-        <div class="bg-white dark:bg-gray-800 overflow-hidden border rounded-lg p-4">
-        </div>
-    </div>
-</x-app-layout>
---
+++ resources/views/livewire/admin/index.blade.php
@@ -0,0 +1,10 @@
+<div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+    <h2 class="text-xl text-gray-800 dark:text-gray-200 leading-tight mb-5">
+        {{ __('Dashboard') }}
+    </h2>
+    <div class="bg-white dark:bg-gray-800 overflow-hidden border">
+        <div class="p-6 text-gray-900 dark:text-gray-100">
+            {{ __("You're logged in!") }}
+        </div>
+    </div>
+</div>
